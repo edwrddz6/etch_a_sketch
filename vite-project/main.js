2176598d95b6de4675be981b617b5b7fee371390
Edward Diaz <diazedward24@gmail.com>
2023-12-27 15:25:41 -0800

    Add functions and event listeners to eraser and clear

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,10 +1,38 @@
 const squaresContainer = document.getElementById('squares');
+const clearButton = document.getElementById('clear');
+const eraserButton = document.getElementById('eraser');
 
-for (let i = 0; i< 16 * 16; i++) {
+let eraserMode = false;
+
+function createGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
     const newDiv = document.createElement('div');
     squaresContainer.appendChild(newDiv);
 
-    newDiv.addEventListener('mouseover', function() {
-      newDiv.style.backgroundColor = 'black';
+    newDiv.addEventListener('mouseover', function(event) {
+      if (eraserMode) {
+        event.target.style.backgroundColor = 'lightgrey';
+      } else {
+        event.target.style.backgroundColor = 'darkgray';
+      }
     });
+  }
 }
+
+createGrid();
+
+function toggleEraserMode() {
+  eraserMode = !eraserMode;
+  eraserButton.classList.toggle('active');
+}
+
+clearButton.addEventListener('click', function() {
+  const gridSquares = document.querySelectorAll('#squares div');
+  gridSquares.forEach(square => {
+    square.style.backgroundColor = 'lightgray';
+  });
+  eraserMode = false;
+  eraserButton.classList.remove('active');
+});
+
+eraserButton.addEventListener('click', toggleEraserMode);
