4b0dbfa33060892ebcaf971664c49b6d1acd5320
Edward Diaz <diazedward24@gmail.com>
2023-12-27 11:17:04 -0800

    Append div elements, use for loop to create squares

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const squaresContainer = document.getElementById('squares');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+for (let i = 0; i< 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    
+    const newDiv = document.createElement('div');
 
-setupCounter(document.querySelector('#counter'))
+    newDiv.style.width = '30px';
+    newDiv.style.height = '30px';
+    newDiv.style.backgroundColor = 'lightgrey';
+
+    newDiv.style.border = '1px solid white';
+
+    squaresContainer.appendChild(newDiv);
+  }
+}
